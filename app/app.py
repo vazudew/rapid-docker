412b58acc9fab45e2c15bcdabc3acece2083db75
techzen <vazudew@gmail.com>
2020-10-08 19:00:20 +0200

    Create app.py

---
+++ app/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, redirect, url_for, render_template, request, flash
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template('index.html')
+
+def printHello():
+    print("Hello World!")
+
+if __name__ == '__main__':
+    app.run(debug=False,host='127.0.0.1')
